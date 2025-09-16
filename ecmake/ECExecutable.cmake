723d31fc8deb0e8f8aa8edafd4f3bf82489fe85a
R533-Code <raphaeldibnehmepc@gmail.com>
2025-09-16 17:48:06 +0200

    Added support for NO_DEBUG_POSTFIX

--- ecmake/ECExecutable.cmake
+++ ecmake/ECExecutable.cmake
@@ -3,12 +3,11 @@ include_guard(GLOBAL)
 include(ECMake)
 include(ECNamespace)
 include(ECParseArgs)
-include(GNUInstallDirs)
 
 function(ec_add_executable NAME)
     ec_parse_with_defaults(EXEC
         "VERSION;0.0.0.0;ROOT_DIR;${CMAKE_CURRENT_SOURCE_DIR};CXX_VERSION;20;INSTALL_BINDIR;${CMAKE_INSTALL_BINDIR};INSTALL_COMPONENT;Runtime" # defaults
-        "NO_CONFIG;NO_INSTALL;NO_PIC;NO_CONFORMANT_PREPROCESSOR_MSVC" # options
+        "NO_CONFIG;NO_INSTALL;NO_PIC;NO_CONFORMANT_PREPROCESSOR_MSVC;NO_DEBUG_POSTFIX" # options
         "VERSION;ROOT_DIR;CXX_VERSION" # one value
         "" # multi value
         ${ARGN}
@@ -44,6 +43,8 @@ function(ec_add_executable NAME)
     )
 
     if(NOT EXEC_NO_INSTALL)
+        include(GNUInstallDirs)
+
         # install the target
         install(TARGETS ${NAME}
             RUNTIME DESTINATION ${EXEC_INSTALL_BINDIR}
