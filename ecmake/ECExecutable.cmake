efc65be903e6ecd1c575aae39e4d2670801583ee
R533-Code <raphaeldibnehmepc@gmail.com>
2025-09-16 09:14:58 +0200

    Added install support for executables

--- ecmake/ECExecutable.cmake
+++ ecmake/ECExecutable.cmake
@@ -3,11 +3,12 @@ include_guard(GLOBAL)
 include(ECMake)
 include(ECNamespace)
 include(ECParseArgs)
+include(GNUInstallDirs)
 
 function(ec_add_executable NAME)
     ec_parse_with_defaults(EXEC
-        "VERSION;0.0.0.0;ROOT_DIR;${CMAKE_CURRENT_SOURCE_DIR};CXX_VERSION;20" # defaults
-        "NO_CONFIG" # options
+        "VERSION;0.0.0.0;ROOT_DIR;${CMAKE_CURRENT_SOURCE_DIR};CXX_VERSION;20;INSTALL_BINDIR;${CMAKE_INSTALL_BINDIR};INSTALL_COMPONENT;Runtime" # defaults
+        "NO_CONFIG;NO_INSTALL" # options
         "VERSION;ROOT_DIR;CXX_VERSION" # one value
         "" # multi value
         ${ARGN}
@@ -24,7 +25,7 @@ function(ec_add_executable NAME)
     file(GLOB_RECURSE _exec_cpp "${EXEC_ROOT_DIR}/src/*.cpp")
     file(GLOB_RECURSE _exec_hpp "${EXEC_ROOT_DIR}/src/*.h" "${EXEC_ROOT_DIR}/src/*.hpp")
 
-    add_executable(${NAME} ${_exec_cpp} ${_exec_h})
+    add_executable(${NAME} ${_exec_cpp} ${_exec_hpp})
     add_executable(${NAME_ALIAS} ALIAS ${NAME})
 
     set_target_properties(${NAME} PROPERTIES
@@ -78,6 +79,14 @@ function(ec_add_executable NAME)
     if(MSVC)
         target_compile_options(${NAME} PRIVATE "/Zc:preprocessor")
         message(VERBOSE "Adding conformant preprocessor (for MSVC)")
+    endif()
+
+    if(NOT EXEC_NO_INSTALL)
+        install(TARGETS ${NAME}
+            RUNTIME DESTINATION ${EXEC_INSTALL_BINDIR}
+            BUNDLE DESTINATION ${EXEC_INSTALL_BINDIR}
+            COMPONENT ${EXEC_INSTALL_COMPONENT}
+        )
     endif()
 
     message(VERBOSE "Created executable ${NAME_ALIAS}.")
