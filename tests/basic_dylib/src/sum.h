27eceebbad50d5ef5812f69b0ea047e01dc12015
R533-Code <raphaeldibnehmepc@gmail.com>
2025-09-16 17:57:52 +0200

    Fixed export macro and include

--- tests/basic_dylib/src/sum.h
+++ tests/basic_dylib/src/sum.h
@@ -1,11 +1,11 @@
 #ifndef __EC_HG_TESTS_BASIC_DYLIB
 #define __EC_HG_TESTS_BASIC_DYLIB
 
-#include <basic_dylib_export.h>
+#include <tests_basic_dylib_export.h>
 
 namespace test::ec
 {
-    BASIC_DYLIB_EXPORT
+    TESTS_BASIC_DYLIB_EXPORT
     int sum(int a, int b) noexcept;
 }
 
