327a62ba6019d68b0be711a681238dca01f66692
R533-Code <raphaeldibnehmepc@gmail.com>
2025-09-17 11:03:37 +0200

    Added static library test

---
+++ tests/basic_stlib/src/sum.cpp
@@ -0,0 +1,9 @@
+#include "sum.h"
+
+namespace test::ec
+{
+    int sum(int a, int b) noexcept
+    {
+        return a + b;
+    }
+}
---
+++ tests/basic_stlib/src/sum.h
@@ -0,0 +1,9 @@
+#ifndef __EC_HG_TESTS_BASIC_STLIB
+#define __EC_HG_TESTS_BASIC_STLIB
+
+namespace test::ec
+{
+    int sum(int a, int b) noexcept;
+}
+
+#endif // !__EC_HG_TESTS_BASIC_STLIB
