46d8363efd5a97182b1e54ab86ce26c7134905a2
R533-Code <raphaeldibnehmepc@gmail.com>
2025-09-17 20:09:19 +0200

    Added errors for missing keys of defaults

--- ecmake/ECParseArgs.cmake
+++ ecmake/ECParseArgs.cmake
@@ -8,7 +8,7 @@ include_guard(GLOBAL)
 # OPTIONS: The options (same as for `cmake_parse_arguments`)
 # ONE_VALUE: The single value options (same as for `cmake_parse_arguments`)
 # MULTI_VALUE: The multi-value options (same as for `cmake_parse_arguments`)
-function(ec_parse_with_DEFAULTS PREFIX DEFAULTS OPTIONS ONE_VALUE MULTI_VALUE)
+function(ec_parse_with_defaults PREFIX DEFAULTS OPTIONS ONE_VALUE MULTI_VALUE)
     set(_DEFAULTS ${DEFAULTS})
     list(LENGTH _DEFAULTS _len)
 
@@ -27,6 +27,43 @@ function(ec_parse_with_DEFAULTS PREFIX DEFAULTS OPTIONS ONE_VALUE MULTI_VALUE)
             set(_def_${_k} "${_v}")
         endforeach()
     endif()
+
+    set(_DEFAULT_KEYS)
+    list(LENGTH _DEFAULTS _len)
+
+    if(_len GREATER 0)
+        math(EXPR _last "${_len}-1")
+
+        foreach(i RANGE 0 ${_last} 2)
+            list(GET _DEFAULTS ${i} _k)
+            math(EXPR _j "${i}+1")
+
+            if(_j GREATER _last)
+                message(FATAL_ERROR "Defaults must be KEY;VALUE pairs. Missing value for '${_k}'.")
+            endif()
+
+            list(GET _DEFAULTS ${_j} _v)
+            set(_def_${_k} "${_v}")
+            list(APPEND _DEFAULT_KEYS "${_k}")
+        endforeach()
+    endif()
+
+    set(_VALUE_KEYS ${ONE_VALUE} ${MULTI_VALUE})
+
+    foreach(_k IN LISTS _DEFAULT_KEYS)
+        list(FIND _VALUE_KEYS "${_k}" _hit)
+
+        if(_hit EQUAL -1)
+            list(FIND OPTIONS "${_k}" _is_opt)
+
+            if(_is_opt GREATER -1)
+                message(FATAL_ERROR "Default provided for OPTION '${_k}'. Options are boolean and cannot have defaults.")
+            else()
+                message(FATAL_ERROR
+                    "Default provided for undeclared key '${_k}'. Add it to ONE_VALUE or MULTI_VALUE, or remove the default.")
+            endif()
+        endif()
+    endforeach()
 
     cmake_parse_arguments(PARSE "${OPTIONS}" "${ONE_VALUE}" "${MULTI_VALUE}" ${ARGN})
 
