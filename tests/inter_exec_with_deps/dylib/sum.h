d6a357fba3ff146daeeb4463572292a0ca08b0ce
R533-Code <raphaeldibnehmepc@gmail.com>
2025-09-28 22:53:13 +0200

    Removed src/

--- tests/inter_exec_with_deps/dylib/sum.h
+++ tests/inter_exec_with_deps/dylib/sum.h
@@ -1,11 +1,11 @@
 #ifndef __HG_INTER_EXEC_DYLIB
 #define __HG_INTER_EXEC_DYLIB
 
-#include <tests_inter_exec_with_deps_dylib_export.h>
+#include <tests_inter_dylib_export.h>
 
 namespace test
 {
-  TESTS_INTER_EXEC_WITH_DEPS_DYLIB_EXPORT
+  TESTS_INTER_DYLIB_EXPORT
   int sum(int a, int b, int c);
 }
 
